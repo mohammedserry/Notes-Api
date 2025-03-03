64f41c76fc3d47b363620acc7b0ba2ccbcab17d0
mohammedserry <mohammedserry582@gmail.com>
2025-03-03 10:59:09 +0200

    fix

--- config/db.js
+++ config/db.js
@@ -1,12 +1,14 @@
-const mongoose = require('mongoose');
+require('dotenv').config();
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    const conn = await mongoose.connect(process.env.MONGODB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
-  
+  } catch (error) {
+    console.error(`MongoDB Connection Error: ${error.message}`);
+    process.exit(1); // Exit process with failure
+  }
 };
 
 module.exports = connectDB;
